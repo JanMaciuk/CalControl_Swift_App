fe1f4dab65e6b8e9321ab6f9083324aa92e8496e
JakubG <kuba.ganko@o2.pl>
2024-10-24 16:23:12 +0200

    Initial Commit

---
+++ CalControl/CalControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalControlApp.swift
+//  CalControl
+//
+//  Created by MacbookGanko on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalControl
+//
+//  Created by MacbookGanko on 24/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
