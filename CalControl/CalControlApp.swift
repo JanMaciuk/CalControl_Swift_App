42e203fed885393910d20c193b47254d90710e7d
J M <272664@student.pwr.edu.pl>
2024-10-24 15:43:46 +0200

    Initial Commit

---
+++ CalControl/CalControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  CalControlApp.swift
+//  CalControl
+//
+//  Created by stud on 24/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CalControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CalControlTests/CalControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalControlTests.swift
+//  CalControlTests
+//
+//  Created by stud on 24/10/2024.
+//
+
+import Testing
+@testable import CalControl
+
+struct CalControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
